5d4b06af6332e0aa55159a6096b9d2809857d04c
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-04-07 16:10:12 -0500

    Third commit, skeleton almost done.

--- static/js/main.js
+++ static/js/main.js
@@ -7,29 +7,25 @@ function clickedOn(){
   .then(function(response){
     return response.json();
   })
-  .then(function(campgrounds){
-    var camps = [];
-    for(let i of campgrounds.data){
-      camp_name = i.name;
-      long_lat = i.latLong;
-      description = i.description;
-      if (long_lat !== "" && camp_name !== "") {
-        camp = i.latLong.split(":")
+  .then(function(parks){
+    var all_parks = [];
+    for(let park of parks.data){
+      let park_name = park.name,
+          long_lat = park.latLong,
+          description = park.description;
+      if (long_lat !== "" && park_name !== "") {
+        let camp = park.latLong.split(":")
         let long = camp[1].split(",")[0];
         let lat = camp[2].split("}")[0];
-        camps.push([camp_name,long,lat, description])
+        all_parks.push([park_name,long,lat, description])
       }
     }
-    console.log(camps);
-    for (let camp of camps) {
-      let paragraph = document.createElement("p");
-      paragraph.textContent = camp[0]
-      document.body.appendChild(paragraph)
-    }
-  return camps})
+    console.log(all_parks);
+    // for (let camp of all_parks) {
+    //   let paragraph = document.createElement("p");
+    //   paragraph.textContent = camp[0]
+    //   document.body.appendChild(paragraph)
+    // }
+  return all_parks})
   .then(plotMarkers)
-}
-
-function clickedOnCamp() {
-  //
 }
--- static/js/map.js
+++ static/js/map.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     else
       lang = 'en';
 
-    var js_file = document.createElement('script');
+    let js_file = document.createElement('script');
     js_file.type = 'text/javascript';
     js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyA8G8oMHHJExl4LvmX3LpCWgwQVi7KeZow&language=' + lang;
     document.getElementsByTagName('head')[0].appendChild(js_file);
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-var map;
-var markers = [];
+let map;
+let markers = [];
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:40, lng:-90},
@@ -26,28 +26,37 @@ function initMap() {
 function plotMarkers(m){
   deleteMarkers();
   let bounds;
-  // markers = [];
   bounds = new google.maps.LatLngBounds();
 
   m.forEach(function (marker) {
-    var position = new google.maps.LatLng(marker[1], marker[2]);
+    let position = new google.maps.LatLng(marker[1], marker[2]);
 
-    markers.push(
-      new google.maps.Marker({
-        position: position,
-        title: marker[0],
-        map: map,
-        animation: google.maps.Animation.DROP
-      })
-    );
+
+    let newMarker = new google.maps.Marker({
+      position: position,
+      title: marker[0],
+      map: map,
+      animation: google.maps.Animation.DROP
+    })
+    let content = "Loan Number:  ";
+
+    let infowindow = new google.maps.InfoWindow()
+
+    google.maps.event.addListener(newMarker,'click', (function(newMarker,content,infowindow){
+      return function() {
+         infowindow.setContent(content);
+         infowindow.open(map,newMarker);
+      };
+  })(newMarker,content,infowindow));
 
     bounds.extend(position);
+    markers.push(newMarker)
   });
   map.fitBounds(bounds);
 }
 
 function deleteMarkers() {
-    for (var i = 0; i < markers.length; i++) {
+    for (let i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
     markers = [];
