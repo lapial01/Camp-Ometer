fc7f42c3620f78a114977a64566fb842c3e405f8
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-04-09 16:02:12 -0500

    Fixed undefined card

--- static/js/weather.js
+++ static/js/weather.js
@@ -16,7 +16,7 @@ function fiveDayWeather(lst) {
             five_days[day_dt_txt[0]]["description"] = day.weather[0]["description"]
         }
     }
-    displayCards()
+    displayCards();
     populateCards(five_days)
 }
 
@@ -29,26 +29,32 @@ function displayCards() {
 
 function populateCards(obj) {
     let eachcard_title = document.querySelectorAll(".title")
-      , allcards = document.querySelectorAll(".card")
       , icons = document.querySelectorAll(".weather_icon")
       , temps = document.querySelectorAll(".desc");
 
-    let i = 0, max = -50, chosen;
+    var allcards = document.querySelectorAll(".card");
 
-    for (let item in obj) {
-        allcards[i].style.background = "#2b2727";
-        if (obj[item]["temp_max"] > max) {
+    let i = 0, max = -50, chosen;
+      for (let item in obj) {
+        if (i<=Object.keys(obj).length) {
+          allcards[i].style.background = "#2b2727";
+          console.log(i);
+          if (obj[item]["temp_max"] > max) {
             max = obj[item]["temp_max"]
             chosen = allcards[i]
+          }
+          let split_date = item.split("-")
+          eachcard_title[i].textContent = split_date[1] + "/" + split_date[2]
+          eachcard_title[i].style.color = "white";
+          icons[i].src = "http://openweathermap.org/img/w/" + obj[item]["icon"] + ".png"
+          icons[i].title = obj[item]["description"]
+          temps[i].textContent = "Max temperature: " + obj[item]["temp_max"] + "°F"
+          i++;
+
         }
-        let split_date = item.split("-")
-        eachcard_title[i].textContent = split_date[1] + "/" + split_date[2]
-        eachcard_title[i].style.color = "white";
-        icons[i].src = "http://openweathermap.org/img/w/" + obj[item]["icon"] + ".png"
-        icons[i].title = obj[item]["description"]
-        temps[i].textContent = "Max temperature: " + obj[item]["temp_max"] + "°F"
-        i++;
-    }
+      }
+
+
     console.log(max);
     console.log(chosen);
     chosen.style.background = "#00C851";
