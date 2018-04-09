65552a21b4ea33359773aebee89fb2c4a4271b25
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-04-08 22:52:38 -0500

    Integrated with weather API

--- static/js/map.js
+++ static/js/map.js
@@ -1,68 +1,71 @@
-document.addEventListener('DOMContentLoaded', function () {
-  if (document.querySelectorAll('#map').length > 0)
-  {
-    if (document.querySelector('html').lang)
-      lang = document.querySelector('html').lang;
-    else
-      lang = 'en';
+document.addEventListener('DOMContentLoaded', function() {
+    if (document.querySelectorAll('#map').length > 0) {
+        if (document.querySelector('html').lang)
+            lang = document.querySelector('html').lang;
+        else
+            lang = 'en';
 
-    let js_file = document.createElement('script');
-    js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyA8G8oMHHJExl4LvmX3LpCWgwQVi7KeZow&language=' + lang;
-    document.getElementsByTagName('head')[0].appendChild(js_file);
-  }
+        let js_file = document.createElement('script');
+        js_file.type = 'text/javascript';
+        js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyA8G8oMHHJExl4LvmX3LpCWgwQVi7KeZow&language=' + lang;
+        document.getElementsByTagName('head')[0].appendChild(js_file);
+    }
 });
-
 
 let map;
 let markers = [];
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:40, lng:-90},
-    zoom: 4
-  });
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: {
+            lat: 40,
+            lng: -100
+        },
+        zoom: 3
+    });
 }
 
-function plotMarkers(m){
-  deleteMarkers();
-  let bounds;
-  bounds = new google.maps.LatLngBounds();
+function plotMarkers(m) {
+    deleteMarkers();
+    let bounds;
+    bounds = new google.maps.LatLngBounds();
 
-  m.forEach(function (marker) {
-    let position = new google.maps.LatLng(marker[1], marker[2]);
+    m.forEach(function(marker) {
+        let position = new google.maps.LatLng(marker[1],marker[2]);
+        let newMarker = new google.maps.Marker({
+            position: position,
+            title: marker[0] + " National Park",
+            map: map,
+            animation: google.maps.Animation.DROP
+        })
 
+        let content = '<div class="info-window">' + '<h3 id="park_map">' + marker[0] + ' National Park.</h3>' + '<div class="bodyContent">' +
+        '<p style="color:black;font-size:12px;">' + marker[3] + '</p>' +
+        '<span id="coord" style="display:none;">{"lat": ' + marker[1] + ', "lon": ' + marker[2] + '}</span>' +
+        '</div>' + '<div class="info-content">' + '<span> Web: ' + '<a href="' + marker[4] + '";font-size:10px;>' + marker[0] + '</a></span></p>' + '</div>' +
+        '</div>'
 
-    let newMarker = new google.maps.Marker({
-      position: position,
-      title: marker[0],
-      map: map,
-      animation: google.maps.Animation.DROP
-    })
-    let content = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">'+marker[0]+'</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>'+marker[0]+" "+marker[3]'</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '</p>'+
-            '</div>'+
-            '</div>';
+        let infowindow = new google.maps.InfoWindow()
 
-    let infowindow = new google.maps.InfoWindow()
+        google.maps.event.addListener(newMarker, 'click', (function(newMarker, content, infowindow) {
+            return function() {
+                infowindow.setContent(content);
+                infowindow.setOptions({maxWidth:350})
+                infowindow.open(map, newMarker);
+                let parser = new DOMParser();
+                console.log(infowindow.content);
+                let doc = parser.parseFromString(infowindow.content, "application/xml");
+                let cont = JSON.parse(doc.getElementById("coord").textContent)
+                let park_from_map = doc.querySelector("#park_map").textContent
+                setTitle(park_from_map)
+                getForecast(cont);
+            };
+        }
+        )(newMarker, content, infowindow));
 
-    google.maps.event.addListener(newMarker,'click', (function(newMarker,content,infowindow){
-      return function() {
-         infowindow.setContent(content);
-         infowindow.open(map,newMarker);
-      };
-  })(newMarker,content,infowindow));
-
-    bounds.extend(position);
-    markers.push(newMarker)
-  });
-  map.fitBounds(bounds);
+        bounds.extend(position);
+        markers.push(newMarker)
+    });
+    map.fitBounds(bounds);
 }
 
 function deleteMarkers() {
@@ -70,4 +73,4 @@ function deleteMarkers() {
         markers[i].setMap(null);
     }
     markers = [];
-  };
+};
