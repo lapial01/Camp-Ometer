2a1377cbc717d31eaf76c6a0351e1183273f8bfb
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-04-08 22:50:19 -0500

    Added weather route

--- campOmeter.py
+++ campOmeter.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Response, request, redirect, url_for, render_template
+from flask import Flask, Response, request, redirect, url_for, render_template
 import requests
 import json
 
@@ -19,5 +19,12 @@ def parksInState():
     res.headers["Content-type"] = "application/json"
     return res
 
+@app.route("/getweather")
+def weatherForPark():
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+    res = Response(requests.get("https://api.openweathermap.org/data/2.5/forecast/?lat="+lat+"&lon="+lon+"&units=imperial&appid=a555a16eae579a59cfddfa0dd3d7e884").text)
+    res.headers["Content-type"] = "application/json"
+    return res
 
 app.run(debug=True,port=5001)
